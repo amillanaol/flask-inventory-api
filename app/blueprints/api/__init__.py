4e0a88977dd4a457a102e78adaea259ad2d79215
amillanaol <alexis.millanao@hotmail.com>
2026-03-27 15:07:19 -0300

    feat(api): agregar blueprint de API
    - Se crea el archivo __init__.py para el blueprint de API
    - Se importan los módulos de productos, categorías y movimientos

---
+++ app/blueprints/api/__init__.py
@@ -0,0 +1,5 @@
+from app.blueprints.api.products import api_products
+from app.blueprints.api.categories import api_categories
+from app.blueprints.api.movements import api_movements
+
+__all__ = ["api_products", "api_categories", "api_movements"]
